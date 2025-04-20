83831bffcfe42c4c447319b5d2977cf47f2b6455
Jan Hohenheim <jan@hohenheim.ch>
2025-04-20 16:59:59 +0200

    Add hex values to docs

--- src/theme/palette.rs
+++ src/theme/palette.rs
@@ -1,10 +1,18 @@
 use bevy::prelude::*;
 
+
+/// #2f5392
 pub const BUTTON_HOVERED_BACKGROUND: Color = Color::srgb(0.186, 0.328, 0.573);
+/// #4879c5
 pub const BUTTON_PRESSED_BACKGROUND: Color = Color::srgb(0.286, 0.478, 0.773);
 
+/// #ebebeb
 pub const BUTTON_TEXT: Color = Color::srgb(0.925, 0.925, 0.925);
-pub const LABEL_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
-pub const HEADER_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
 
-pub const NODE_BACKGROUND: Color = Color::srgb(0.286, 0.478, 0.773);
+/// #868686
+pub const LABEL_TEXT: Color = Color::srgb(0.529, 0.529, 0.529);
+/// #868686
+pub const HEADER_TEXT: Color = Color::srgb(0.529, 0.529, 0.529);
+
+/// #2b2c2e
+pub const SCREEN_BACKGROUND: Color = Color::srgb(0.169, 0.173, 0.184);
